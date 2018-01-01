4cbc2daf1561a39fae501ab99759077f8d480fd1
dpmlanonymous <dpmlalgorithms@gmail.com>
2018-01-02 06:21:46 +0800

    initial push

---
+++ common/noise.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+
+# TODO: document and fix
+def compute_gamma_noise(priv_param, data_dimensions, max_norm):
+    gamma_r = np.random.gamma(data_dimensions, max_norm / priv_param)
+    spherical_random_vec = np.random.normal(0, 1.0, data_dimensions)
+    return (gamma_r*spherical_random_vec) / (
+        np.linalg.norm(spherical_random_vec))
